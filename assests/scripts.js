7dd345d897e302d92a087b05bec9898b6264c181
Sriram Sritha <srithasriram30@gmail.com>
2021-05-23 10:33:59 +0800

    Seperate content into different pages

---
+++ assests/scripts.js
@@ -0,0 +1,10 @@
+function sendMail() {
+    var emailAddr = document.getElementById('emailInput').value;
+    var message = document.getElementById('msg').value;
+
+    console.log(msg);
+}
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
